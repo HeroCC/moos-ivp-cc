0c10333466598c422af04d8b96cbd76509b0b730
Conlan Cesar <conlanc@csail.mit.edu>
2020-03-19 20:12:39 -0400

    More configurables for SeeByte mission

--- missions/seebyte/launch.sh
+++ missions/seebyte/launch.sh
@@ -10,9 +10,10 @@
 trap "pkill -INT -P $$" EXIT SIGTERM SIGHUP SIGINT SIGKILL
 TIME_WARP=${TIME_WARP:-1}
 COMMUNITY="seebyte"
-SIM="${SIM:-no}"
 NMEA_HOST="${NMEA_HOST:-localhost}"
 NMEA_PORT="${NMEA_PORT:-10110}"
+NMEA_CHECKSUM="${NMEA_VALIDATE_CHECKSUM:-true}"
+NMEA_TIME_DELTA="${NMEA_TIMESTAMP_DELTA:-3}"
 HERON_HOST="${HERON_HOST:-192.168.10.1}"
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -23,12 +24,22 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 for ARGI; do
   if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ]; then
   	echo "launch.sh [SWITCHES] [time_warp]   "
-  	echo "  --help, -h                       " 
+  	echo "  --help, -h                       "
     echo "  --nogui                          "
+    echo "  --nochecksum  - don't check NMEA checksum"
+    echo "  --notimestamp - don't check NMEA timestamp"
+    echo "  --novalidate  - don't check NMEA checksum or timestamp"
     echo "  --sim, -s                        "
 	  exit 0
   elif [ "${ARGI}" = "--nogui" ]; then
     unset DISPLAY
+  elif [ "${ARGI}" = "--nochecksum" ]; then
+    NMEA_CHECKSUM="false"
+  elif [ "${ARGI}" = "--notimestamp" ]; then
+    NMEA_TIME_DELTA="-1"
+  elif [ "${ARGI}" = "--novalidate" ]; then
+    NMEA_VALIDATE_CHECKSUM="false"
+    NMEA_TIMESTAMP_DELTA="-1"
   elif [ "${ARGI}" = "--sim" ] || [ "${ARGI}" = "-s" ]; then
     SIM="yes"
   elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$TIME_WARP" = 1 ]; then 
@@ -46,7 +57,8 @@ mkdir -p logs/
 nsplug $COMMUNITY.moos targ_${COMMUNITY}.moos -f \
   DISPLAY=$DISPLAY WARP=$TIME_WARP COMMUNITY=$COMMUNITY \
   SIM=$SIM HERON_HOST=$HERON_HOST \
-  NMEA_HOST=$NMEA_HOST NMEA_PORT=$NMEA_PORT
+  NMEA_HOST=$NMEA_HOST NMEA_PORT=$NMEA_PORT \
+  NMEA_TIME_DELTA=$NMEA_TIME_DELTA NMEA_CHECKSUM=$NMEA_CHECKSUM
 
 #----------------------------------------------------------
 #  Part 4: Launch the processes
