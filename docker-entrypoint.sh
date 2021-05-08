402a3848ce9740f2383be57e8c5ef95b68de604d
Conlan Cesar <conlanc@csail.mit.edu>
2021-05-07 21:08:14 -0400

    Add $LAUNCH_TYPE variable for docker entrypoint
    Looks for a launch script resembling launch_${LAUNCH_TYPE}.sh in the
    appropriate missions folder.
    
    Will allow for better shoreside behavior / different launch scripts down the line.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e
 
-echo "DEP: Staring mission with Docker entrypoint script"
-echo "DEP: To bypass this and drop into bash, add 'bash' to the end of the command you just ran"
+echo "Entrypoint: Starting mission with Docker entrypoint script"
 
 # CD to location of entrypoint script (should be root of tree)
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [ -z "$MISSION" ]; then
-  echo "DEP: \$MISSION is unset! Please set to the mission you'd like to run"
+  echo "Entrypoint: \$MISSION is unset! Please set to the mission you'd like to run"
   exit 1
 fi
 
@@ -15,16 +14,19 @@ fi
 if [ -d "missions/$MISSION" ]; then
   cd "missions/$MISSION"
 else
-  echo "DEP: Unable to find mission by name '$MISSION'"
+  echo "Entrypoint: Unable to find mission by name '$MISSION'"
   exit 1
 fi
 
 # Launch the mission with all aditional args, or fail
-if [ -x "launch.sh" ]; then
+if [ -n "$LAUNCH_TYPE" ] && [ -x "launch_${LAUNCH_TYPE}.sh" ]; then
+  echo "Entrypoint: LAUNCH_TYPE specified -- this will be a ${LAUNCH_TYPE} container"
+  ./launch_${LAUNCH_TYPE}.sh "$@"
+elif [ -x "launch.sh" ]; then
   ./launch.sh "$@"
-  echo "DEP: Goodbye!"
 else
-  echo "DEP: Unable to find a 'launch.sh' script in mission '$MISSION'"
+  echo "Entrypoint: Unable to find an appropriate launch script in mission '$MISSION'"
+  echo "Entrypoint: Ensure a launch.sh or launch_\${LAUNCH_TYPE}.sh executable exist"
   exit 1
 fi
 
