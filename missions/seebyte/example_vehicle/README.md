7c8d26c72c47a97fe8a6333baeb266d9d2dee262
Conlan Cesar <conlanc@csail.mit.edu>
2022-05-18 16:15:17 -0400

    Neptune: support for external vehicle driver comms
    Right now to run a real vehicle, we need to run MOOS natively, or edit the docker image to contain the vehicle driver.
    To resolve this, we forward the DESIRED_* mailings to an arbitrary community -- ideally only containing vehicle & sensor drivers. That community will recieve our DESIRED information, and send us back corresponding NAV_* and sensor messages.
    This allows us to keep the Neptune image untouched, and run the container on any vehicle without major changes to the host or our image.

---
+++ missions/seebyte/example_vehicle/README.md
@@ -0,0 +1,3 @@
+This is a skeleton mission, which you should replace with a full mission with all of your vehicle-side drivers -- sensors, vehicle drivers & interfaces, etc.
+
+For example, replace uSimMarine with iM300, and the pShare host and port with the corresponding main vehicle community (possibly inside of docker) to ensure the Neptune MOOS environment is contained, and the vehicle drivers can be abstracted away from it.
--- missions/seebyte/launch_vehicle.sh
+++ missions/seebyte/launch_vehicle.sh
@@ -16,7 +16,7 @@ NMEA_PORT="${NMEA_PORT:-10110}"
 NMEA_CHECKSUM="${NMEA_CHECKSUM:-true}"
 NMEA_TIME_DELTA="${NMEA_TIME_DELTA:-3}"
 SHORE_PORT="${SHORE_PORT:-9300}"
-HERON_HOST="${HERON_HOST:-192.168.10.1}"
+VEHICLE_ROUTE="${VEHICLE_ROUTE}"
 PSHARE_PORT="${PSHARE_PORT:-9305}"
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -35,6 +35,7 @@ for ARGI; do
     echo "  --notimestamp - don't check NMEA timestamp"
     echo "  --novalidate  - don't check NMEA checksum or timestamp"
     echo "  --shore=HOST[:PORT]              "
+    echo "  --vehicle=HOST:PORT - pShare DESIRED_* out to this route"
     echo "  --sim, -s                        "
     echo "  --auto, -a                       "
 	  exit 0
@@ -51,6 +52,8 @@ for ARGI; do
     NMEA_TIME_DELTA="-1"
   elif [ "${ARGI}" = "--sim" ] || [ "${ARGI}" = "-s" ]; then
     SIM="yes"
+  elif [ "${ARGI:0:10}" = "--vehicle=" ] ; then
+    VEHICLE_ROUTE="${ARGI#--vehicle=*}"
   elif [ "${ARGI:0:8}" = "--shore=" ] ; then
     __SHORE_ARG="${ARGI#--shore=*}"
     IFS=':' read -ra __SHORE <<< "$__SHORE_ARG"
@@ -88,7 +91,7 @@ fi
 mkdir -p logs/
 nsplug meta_seebyte.moos targ_${COMMUNITY}.moos -f \
   DISPLAY=$DISPLAY WARP=$TIME_WARP COMMUNITY=$COMMUNITY \
-  SIM=$SIM HERON_HOST=$HERON_HOST \
+  SIM=$SIM VEHICLE_ROUTE=$VEHICLE_ROUTE \
   SHORE=$SHORE DB_PORT=$DB_PORT \
   PSHARE_PORT=$PSHARE_PORT HOST_IP=$HOST_IP \
   NMEA_HOST=$NMEA_HOST NMEA_PORT=$NMEA_PORT \
